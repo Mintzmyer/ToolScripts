43051f2aecba18ff69b21b25ec571a70f787ed90
Samantha Feinstein <sfeinstein@rosenaviation.com>
2019-01-31 14:56:01 -0800

    Gerrit REST API: Dodged OpenID auth
    Turns out, Gerrit handily has it's own HTTP
    authentication just for the REST API, but
    I did get to learn a lot more about OAuth2
    and OpenID in the process!
    
    Authentication is working, review command is
    working, next steps are to add the logic for
    finding new uncommented reviews

--- Automation/reviewChecklist.py
+++ Automation/reviewChecklist.py
@@ -15,12 +15,48 @@
 #     Review Checklist into our workflow
 
 
-# Public API calls work great, but I'm still having trouble
+# Thanks to this little gem, I finally stopped trying to authenticate
+#     with OpenID and used the HTTP API authentication. *Face palm*
+# https://softwarefactory-project.io/docs/faqs/gerrit_rest_api.html
+aPublic API calls work great, but I'm still having trouble
 # authenticating which prevents me from posting inline comments
 
 from pygerrit2 import GerritRestAPI, HTTPBasicAuth
+from requests_oauthlib import OAuth2
+import os
 
-rest = GerritRestAPI(url='http://gerrit.rosenaviation.com:8080')
+# Get Login Authentication information
+authFilepath = os.path.expanduser('.credentials.txt')
+if os.path.isfile(authFilepath) == False:
+    print("Error: No authentication file found")
+    quit()
+
+with open(authFilepath, 'r') as loginFile:
+    line = loginFile.readline()
+    login = line.split()
+    if len(login) < 2:
+        print("Error: Insufficient login credentials")
+        quit()
+    user = login[0]
+    password = login[1]
+
+# Get Review Guidelines checklist
+guideFilepath = os.path.expanduser('reviewChecklist.txt')
+if os.path.isfile(guideFilepath) == False:
+    print("Error: No reviewChecklist.txt file found")
+    quit()
+
+with open(guideFilepath, 'r') as guideFile:
+    checklist = guideFile.readlines()
+
+
+
+print("Done")
+"""
+
+auth = HTTPBasicAuth(user, password)
+
+rest = GerritRestAPI(url='http://gerrit.rosenaviation.com:8080', auth=auth)
 
 
 change_id = "521627"
@@ -28,18 +64,18 @@ cur_rev = "3"
 
 query = "/changes/" + str(change_id) + "/revisions/" + str(cur_rev) + "/review"
 
-changes = rest.get(query)
-'''
+#changes = rest.get(query)
+#'''
 changes = rest.post(query, json={
     "labels": {
         "Code-Review": +2
     }
 })
-'''
+#'''
 
 
 # changes = rest.get("/changes/?q=status:open")
 print(changes)
 
 print("Done")
-
+"""
